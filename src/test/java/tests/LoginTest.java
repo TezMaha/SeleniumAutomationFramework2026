4048f4076e57e5cac5edecbc2597302922b7f720
TejasviniTest <tejasvinigaikwad241@gmail.com>
2026-06-12 01:30:13 +0530

    First commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+	
+	
+	@Test
+	
+	//create function
+	public void testValidLogin() {
+		
+		LoginPage loginPage = new LoginPage(driver);
+		
+		
+		loginPage.enterUsername("admin@yourstore.com");
+		loginPage.enterPassword("admin");
+		loginPage.clickLogin();
+		
+		System.out.println("Tittle of the page is"+ driver.getTitle());
+		
+		
+		
+	}
+	
+
+}
